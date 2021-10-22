0c1addc7ddf35afe2a36a1518b99406a8ef138b6
cborodi <72648700+cborodi@users.noreply.github.com>
2021-10-22 18:20:22 +0300

    update main

--- Project/main.py
+++ Project/main.py
@@ -1,8 +1,27 @@
 from SymbolTable import SymbolTable
+from PIF import ProgramInternalForm
+from Scanner import generateTokens
+
+if __name__ == '__main__':
+    fileName = input("Scan file> ")
+
+    file = open(fileName, 'r')
+    for line in file:
+        print(line)
+
+    with open(fileName, 'r') as file:
+        for line in file:
+            generateTokens(line)
+
+
+"""
+from SymbolTable import SymbolTable
 
 st = SymbolTable()
 
 st.add("a")
+st.add("ab")
+st.add("ba")
 st.add("b")
 st.add("c")
 st.add("d")
@@ -13,3 +32,6 @@ print(st.get("a"))
 print(st.get("1"))
 print(st.get("b"))
 print(st.get("c"))
+print(st.get("ab"))
+print(st.get("ba"))
+"""
